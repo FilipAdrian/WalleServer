c8a5120b0b22fe10afae83deed7e7e05c6b3fd96
FilipAdrian <adryanfylyp@gmail.com>
2019-01-16 23:25:39 +0200

    create new branch dev

--- src/main/java/com/walle/project/UI/sample/Main.java
+++ src/main/java/com/walle/project/UI/sample/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load (getClass ( ).getClassLoader ().getResource ("Home.fxml"));
+        Parent root = FXMLLoader.load (getClass ( ).getClassLoader ().getResource ("Login.fxml"));
         primaryStage.setTitle ("Login");
         primaryStage.setScene (new Scene (root, 800, 387));
         primaryStage.show ( );
